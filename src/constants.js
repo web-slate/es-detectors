b823b4cca50a09ec54f8ec3f96d952d46b44c0dc
Venkatraman <ramsunvtech@gmail.com>
2020-05-27 23:11:24 +0800

    feat(base): Add Constants File

---
+++ src/constants.js
@@ -0,0 +1,11 @@
+// const IMPORT_DECLARATION = "ImportDeclaration";
+// const IDENTIFIER = "Identifier",
+// const IMPORT_SPECIFIER = "ImportSpecifier";
+// const IMPORT_DEFAULT_SPECIFIER = "ImportDefaultSpecifier";
+
+module.exports = {
+  IMPORT_DECLARATION: 'ImportDeclaration',
+  IDENTIFIER: 'Identifier',
+  IMPORT_SPECIFIER: 'ImportSpecifier',
+  IMPORT_DEFAULT_SPECIFIER: 'ImportDefaultSpecifier',
+};
