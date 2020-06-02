4d02f7d76a342ff10ded987b593b1b57c5486795
Venkatraman <ramsunvtech@gmail.com>
2020-06-03 07:48:20 +0800

    feat(base): Add ES Detector Main Index File

--- src/index.js
+++ src/index.js
@@ -8,6 +8,18 @@ const {
   isImportSpecifier,
 } = require('./Utilities');
 
+const yourCode = `
+import React from 'react';
+import { AgGridReact } from 'ag-grid-react';
+import { AllModules } from '@ag-grid-community/all-modules';
+
+// Ag Grid CSS Files.
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
+
+const Table = () => {}
+`;
+
 // `Detectors` Methods.
 const getImports = require('./getImports/index');
 const getParsedImports = require('./getParsedImports/index');
@@ -16,6 +28,10 @@ const getRawImportInfo = require('./getRawImportInfo/index');
 function detectCode (source) {
   const { body: rawImports } = parse(source, { sourceType: "module" });
   const parsedImportList = [];
+
+  function detectIdentifier() {
+
+  }
 
   rawImports.forEach(importLine => {
     const { type, specifiers, source = {} } = importLine;
