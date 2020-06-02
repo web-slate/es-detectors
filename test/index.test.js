d67804c16123b4e93a5d49c3bcaa630b337fb083
Venkatraman <ramsunvtech@gmail.com>
2020-06-02 10:43:15 +0800

    test(getImports): Add Test for getImports

--- test/index.test.js
+++ test/index.test.js
@@ -19,4 +19,53 @@ describe('es-detector', () => {
   test('should parse the data', () => {
     expect(detector(myEsCode)).not.toBeNull();
   });
+
+  test('should match the import', () => {
+    const yourCode = `
+      import React from 'react';
+      import { AgGridReact } from 'ag-grid-react';
+      import { AllModules } from '@ag-grid-community/all-modules';
+
+      // Ag Grid CSS Files.
+      import 'ag-grid-community/dist/styles/ag-grid.css';
+      import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
+
+      const Table = () => {}
+    `;
+
+    const {
+      getImports,
+    } = detector(yourCode);
+    expect(getImports()).toEqual([
+      {
+        "path": "react",
+        "default": "React",
+        "exported": []
+      },
+      {
+        "path": "ag-grid-react",
+        "default": "",
+        "exported": [
+          "AgGridReact"
+        ]
+      },
+      {
+        "path": "@ag-grid-community/all-modules",
+        "default": "",
+        "exported": [
+          "AllModules"
+        ]
+      },
+      {
+        "path": "ag-grid-community/dist/styles/ag-grid.css",
+        "default": "",
+        "exported": []
+      },
+      {
+        "path": "ag-grid-community/dist/styles/ag-theme-alpine.css",
+        "default": "",
+        "exported": []
+      }
+    ]);
+  });
 })
